ba312a2e6e6333b7934ad4d145f121b315120786
Ayazz04 <ayazmohammed.125@gmail.com>
2025-08-22 02:01:08 +0530

    practice-21-08

--- home prac.py
+++
@@ -1,5 +0,0 @@
-age= 25
-height= 5.11
-name= 'ayaz'
-
-print (name, "is",age, 'years old and', height, "ft tall.")
---
+++ prac.py
@@ -0,0 +1,22 @@
+age= 25
+height= 5.11
+name= 'ayaz'
+
+name= input('enter your name: ')
+gender= input(' enter your gender: ')
+age= int(input('enter your age: '))
+print('hello', name, 'you are a',gender,'and your age is',age,'and next year you will',age+1)
+
+#if/else statement
+
+is_raining = input("Is it raining? (yes/no): ")
+
+if is_raining == "yes":
+    print("Take an umbrella!")
+else:
+    print("Enjoy the sunshine!")
+
+password = (int(input('input your password: ')))
+if password == 2684:
+    print('welcome to the system')
+else: print('incorrect password')
